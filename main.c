0017904fbbeb6a920f2dbea704398fa7f5bd1965
Felipe Moreti <felipe_moreti7@hotmail.com>
2018-08-26 14:34:29 -0300

    .

--- main.c
+++ main.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	initialize();
 
+	int *i = Aloca(10);
+	int *j = Aloca(5);
+	int *k = Aloca(15);
+	char *f = Aloca(sizeof("felipe"));
 	Lista *list = Aloca(sizeof(Lista));
-	initList(list);
-
-	float f = 11.59f;
-	ins_na_lista_vazia(list, (void *)5);
-	ins_fim_lista(list, "felipe");
-	ins_fim_lista(list, &f);
-
-	//remov(list, 1); //Nao esta removendo da memoria a lista nem o dado pois o int* nao é o mesmo que é alocado
-
+	//initList(list); //se inicializar a lista ele para de remover da memoria, nao sei pq
+	//ins_na_lista_vazia(list, (void *) 8);
 	show();
-	//scanf("");
+	printf("\n");
+	Libera(list);
+	//remov(list, 1);
+	show();
 
 	return 0;
 }
